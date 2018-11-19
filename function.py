292791bff645c3d630ccfa6755000464cbd4d98e
emilie.granda <egranda943@gmail.com>
2018-11-19 11:22:58 +0100

    json-server connection

--- function.py
+++ function.py
@@ -1,5 +1,20 @@
+import json
+import requests
 
-class Request():
-    def __init__(self):
-        self.url = "http://localhost:3000/"
-# response = request.get
+class ConnectData():
+
+    def __init__(self, arg):
+
+        self.url = 'http://localhost:3000/api/v1/' + arg
+        response = requests.get(self.url)
+
+        if response.status_code == 200 :
+             print(response.content)
+             print("connect")
+             pass
+
+        else:
+            print('no')
+            print(response.status_code)
+
+ConnectData('indicators/3')
